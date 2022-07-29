6aeb65b58f152d93af5cefa3115e9acb5314dc04
satriyo <satriyo.noto@uii.ac.id>
2022-07-29 08:02:41 +0700

    sql

---
+++ qris_payment.sql
@@ -0,0 +1,156 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: localhost:8889
+-- Generation Time: Jul 29, 2022 at 01:02 AM
+-- Server version: 5.7.34
+-- PHP Version: 8.0.8
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `qris_payment`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `pembayaran`
+--
+
+CREATE TABLE `pembayaran` (
+  `id` int(11) NOT NULL,
+  `nama_pembayaran` varchar(50) NOT NULL,
+  `deskripsi` varchar(200) NOT NULL,
+  `qris` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `pembayaran`
+--
+
+INSERT INTO `pembayaran` (`id`, `nama_pembayaran`, `deskripsi`, `qris`) VALUES
+(3, 'Gopay', 'Arena Of Valor', '22-07-28:12:07:29bananas.jpg'),
+(4, 'Shopee', '', '22-07-29:12:07:31buah.jpg');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `transaksi`
+--
+
+CREATE TABLE `transaksi` (
+  `id_transaksi` int(11) NOT NULL,
+  `pembayaran_id` int(11) NOT NULL,
+  `user_id` int(11) NOT NULL,
+  `no_jenis_transaksi` varchar(50) NOT NULL,
+  `jumlah_bayar` int(11) NOT NULL,
+  `bukti_transfer` varchar(100) NOT NULL,
+  `status` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `transaksi`
+--
+
+INSERT INTO `transaksi` (`id_transaksi`, `pembayaran_id`, `user_id`, `no_jenis_transaksi`, `jumlah_bayar`, `bukti_transfer`, `status`) VALUES
+(1, 3, 2, '1433223423', 10000, '22-07-28:02:07:23bananas.jpg', 'lunas'),
+(8, 3, 2, '43232423', 10000, '22-07-28:02:07:28borobudur1.jpeg', 'proses'),
+(9, 4, 3, '2384832423', 10000, '22-07-28:03:07:29white-bunch-food-nature-fruit.jpg', 'proses'),
+(10, 4, 2, '432334232', 25000, '22-07-29:12:07:22ria.jpeg', 'proses'),
+(11, 4, 2, '55555', 25000, '22-07-29:12:07:14payment.jpg', 'proses');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE `users` (
+  `id` int(11) NOT NULL,
+  `email` varchar(50) NOT NULL,
+  `username` varchar(30) NOT NULL,
+  `password` varchar(100) NOT NULL,
+  `level` varchar(20) NOT NULL,
+  `alamat` text NOT NULL,
+  `no_telp` varchar(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`id`, `email`, `username`, `password`, `level`, `alamat`, `no_telp`) VALUES
+(2, 'admin@gmail.com', 'admin', '81dc9bdb52d04dc20036dbd8313ed055', 'admin', 'Magelang', '01823812312'),
+(3, 'coba@gmail.com', 'coba', '81dc9bdb52d04dc20036dbd8313ed055', 'user', 'Magelang', '38248245235');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `pembayaran`
+--
+ALTER TABLE `pembayaran`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `transaksi`
+--
+ALTER TABLE `transaksi`
+  ADD PRIMARY KEY (`id_transaksi`),
+  ADD KEY `pembayaran_id_2` (`pembayaran_id`),
+  ADD KEY `user_id_2` (`user_id`);
+
+--
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `pembayaran`
+--
+ALTER TABLE `pembayaran`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- AUTO_INCREMENT for table `transaksi`
+--
+ALTER TABLE `transaksi`
+  MODIFY `id_transaksi` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
+
+--
+-- AUTO_INCREMENT for table `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `transaksi`
+--
+ALTER TABLE `transaksi`
+  ADD CONSTRAINT `transaksi_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `transaksi_ibfk_2` FOREIGN KEY (`pembayaran_id`) REFERENCES `pembayaran` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
